714558f27d926562a42c2ba92de42a8b55a17e72
shi.fan <shifan3rd@gmail.com>
2025-09-16 11:16:20 +0000

    asd

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 bazel build -c opt --cxxopt=--std=c++17 --config=android_arm64   --fat_apk_cpu=x86,x86_64,arm64-v8a,armeabi-v7a   --define=android_dexmerger_tool=d8_dexmerger   --define=android_incremental_dexing_tool=d8_dexbuilder   //tensorflow/lite/java:tensorflow-lite-gpu
 bazel build -c opt --cxxopt=--std=c++17 --config=android_arm64   --fat_apk_cpu=x86,x86_64,arm64-v8a,armeabi-v7a   --define=android_dexmerger_tool=d8_dexmerger   --define=android_incremental_dexing_tool=d8_dexbuilder   //tensorflow/lite/java:tensorflow-lite
 
-
+cp bazel-bin/tensorflow/lite/java/tensorflow-lite-gpu.aar . -f
+cp bazel-bin/tensorflow/lite/java/tensorflow-lite.aar . -f
